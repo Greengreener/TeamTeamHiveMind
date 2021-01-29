4393c53ce123cae1b0ef49a43fc98e9dfb82b1d9
Greengreener <36747370+Greengreener@users.noreply.github.com>
2021-01-29 21:43:21 +1100

    StartedPlayer

---
+++ Assets/Scripts/Dino/Dino.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dino : MonoBehaviour
+{
+    [SerializeField] Egg egg;
+    [SerializeField] PlayerInput playerInput;
+    void Start()
+    {
+        egg = GetComponent<Egg>();
+        playerInput = GetComponent<PlayerInput>();
+    }
+}
---
+++ Assets/Scripts/Egg.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Egg : MonoBehaviour
+{
+    [SerializeField] Dino dino;
+    void Start()
+    {
+        dino = GetComponent<Dino>();
+    }
+}
